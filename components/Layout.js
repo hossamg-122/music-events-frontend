3e38c753161e64e2bec54a38dd97c44f4d66ee98
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-06 20:47:04 +0200

    feature : layout page

--- components/Layout.js
+++ components/Layout.js
@@ -3,12 +3,18 @@ import styles from "../styles/Layout.module.css";
 import Meta from "./Meta";
 import Header from "./Header";
 import Footer from "./Footer";
+import ShowCase from "./ShowCase";
+import { useRouter } from "next/router";
 const Layout = ({ title, description, keywords, children }) => {
+    const router = useRouter()
   return (
     <div>
       <Meta title={title} description={description} keywords={keywords} />
       <Header />
+      
       <div className={styles.container}>
+          {router.pathname === "/"&&<ShowCase />}
+      
         <main>{children}</main>
         <Footer />
       </div>
