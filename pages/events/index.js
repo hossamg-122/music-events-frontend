7c6d22adfa1d8321e8d1c423ed905bea529e8fde
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-06 23:47:40 +0200

    feature : render events

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,52 @@
-import React from 'react'
-import Layout from '../../components/Layout'
+import EventItem from "../../components/EventItem";
+import Layout from "../../components/Layout";
+import { API_URL } from "../config";
 
-const EventsPage = () => {
+export default function EventsPage({ events = [] }) {
+  // this console log is at client side
+  // console.log(events)
   return (
-      <Layout  >
-<div>EventsPage</div>
-      </Layout>
-    
-  )
+    <Layout>
+      <div>
+        <h1>Events</h1>
+        {events.length === 0 && <h3>No Events to show</h3>}
+       
+        {events.map((event) => (
+          <EventItem key={event.id} event={event} />
+        ))}
+      </div>
+    </Layout>
+  );
 }
 
-export default EventsPage
+// this code runs on build time and fetch data so we could make static page
+// but there is a side back for this thats if there is any change in data it won't appear in the page as the pase already fetched the exists data during build
+// so to get around this we add revalidate option to props and set it to a specific number of seconds so it will check every that time for any changes in DB and if there
+// any change it will re-fetch the data
+
+export const getStaticProps = async () => {
+  const response = await fetch(`${API_URL}/api/events`);
+  const events = await response.json();
+  // this console log is on server
+  // console.log(events)
+  return {
+    props: {
+      events,
+      revalidate: 1,
+    },
+  };
+};
+
+// this code runs on server each time we request this page
+
+// export const getServerSideProps =async () =>{
+//   const response = await fetch(`${API_URL}/api/events`)
+//   const events = await response.json()
+//   this console log is on server
+//  console.log(events)
+//   return {
+//     props:{
+//       events
+//     }
+//   }
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,53 @@
-
-import Layout from '../components/Layout'
-
-
-
-export default function Home() {
+import EventItem from "../components/EventItem";
+import Layout from "../components/Layout";
+import { API_URL } from "./config";
+import Link from "next/link"
+export default function Home({ events = [] }) {
+  // this console log is at client side
+  // console.log(events)
   return (
-    <Layout >
-<div>
-      Home
-    </div>
+    <Layout>
+      <div>
+        <h1>Upcoming Events</h1>
+        {events.length === 0 && <h3>No Events to show</h3>}
+       
+        {events.map((event) => (
+          <EventItem key={event.id} event={event} />
+        ))}
+        {events.length>0&&<Link href='/events' > <a className="btn-secondary" > View All Events</a></Link>}
+      </div>
     </Layout>
-    
-  )
+  );
 }
+
+// this code runs on build time and fetch data so we could make static page
+// but there is a side back for this thats if there is any change in data it won't appear in the page as the pase already fetched the exists data during build
+// so to get around this we add revalidate option to props and set it to a specific number of seconds so it will check every that time for any changes in DB and if there
+// any change it will re-fetch the data
+
+export const getStaticProps = async () => {
+  const response = await fetch(`${API_URL}/api/events`);
+  const events = await response.json();
+  // this console log is on server
+  // console.log(events)
+  return {
+    props: {
+      events:events.slice(0,3),
+      revalidate: 1,
+    },
+  };
+};
+
+// this code runs on server each time we request this page
+
+// export const getServerSideProps =async () =>{
+//   const response = await fetch(`${API_URL}/api/events`)
+//   const events = await response.json()
+//   this console log is on server
+//  console.log(events)
+//   return {
+//     props:{
+//       events
+//     }
+//   }
+// }
