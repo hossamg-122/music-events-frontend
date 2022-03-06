8887bae30ad2658c2a473b7d981216d45aa1f639
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-06 23:47:13 +0200

    Api config

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+// Api Routes can't be used with next export
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
---
+++ pages/config/index.js
@@ -0,0 +1,4 @@
+
+// we use this environment variable here so if there wasn't a production url we will use local url
+
+export const API_URL = process.env.NEXT_PUPLIC_API_URL ||"http://localhost:3000/"
