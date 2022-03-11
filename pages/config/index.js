43c77953f8b75a505bea434cfa82f2bce5019e06
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-11 23:57:45 +0200

    configuration

--- pages/config/index.js
+++ pages/config/index.js
@@ -1,4 +1,4 @@
 
 // we use this environment variable here so if there wasn't a production url we will use local url
 
-export const API_URL = process.env.NEXT_PUPLIC_API_URL ||"http://localhost:3000/"
+export const API_URL = process.env.NEXT_PUPLIC_API_URL ||"http://localhost:1337"
