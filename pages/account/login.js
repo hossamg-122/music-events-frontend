7b6ed5323b09336404b87e068fabb32a77d6039a
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-16 23:39:46 +0200

    feature : login page

---
+++ pages/account/login.js
@@ -0,0 +1,55 @@
+import React from "react";
+import { FaUser } from "react-icons/fa";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+import styles from "../../styles/AuthForm.module.css";
+import { Formik, Form } from "formik";
+import * as yup from "yup";
+import InputHandler from "../../components/InputHandler"
+const login = () => {
+  const initialValues = {
+    email: "",
+    password: "",
+  };
+  const validate = yup.object().shape({
+    email: yup.string().email().required("required"),
+    password: yup.string().required("required"),
+  });
+  const handleSubmit=(values) => {
+      console.log(values)
+  }
+  return (
+    <Layout title="User Login">
+      <div className={styles.auth}>
+        <h1>
+          <FaUser /> Log In
+        </h1>
+        <ToastContainer />
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validate}
+          onSubmit={(values) => handleSubmit(values)}
+        >
+          {(formValues) => <Form>
+              <InputHandler
+                name='email'
+                label="Email Address"
+                id='email'
+                />
+                <InputHandler
+                name='password'
+                label="Password"
+                id='password'
+                />
+                <input type="submit" value="Login" className={`btn`} />
+              </Form>}
+        </Formik>
+        <p>Don't have an account <Link href="/account/register" >Register</Link></p>
+      </div>
+    </Layout>
+  );
+};
+
+export default login;
