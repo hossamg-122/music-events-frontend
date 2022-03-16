ca90cacf5ff0c8ea755b83d13ab8ae58ed29c25d
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-16 23:39:22 +0200

    fix

--- components/InputHandler.js
+++ components/InputHandler.js
@@ -1,19 +1,32 @@
-import React from 'react'
-import { useField,ErrorMessage } from 'formik'
-import styles from "../styles/Form.module.css"
-const InputHandler = ({label,id,...props}) => {
-    const [field,meta] = useField(props)
-  
-  return (
-     
-     
-    <div>
-    <label className={styles.label} htmlFor={id} >{label}</label>
-    { id==="description"? <textarea className={styles.textarea} id={id}  {...props} {...field}></textarea>: <input className={styles.input} id={id}  {...props} {...field} />}
-    
-    {(meta.touched&&meta.error)&& <div  style={{color:'red'}}><ErrorMessage name={field.name} /></div> }
-</div>
-  )
-}
+import React from "react";
+import { useField, ErrorMessage } from "formik";
+import styles from "../styles/Form.module.css";
+const InputHandler = ({ label, id, ...props }) => {
+  const [field, meta] = useField(props);
 
-export default InputHandler
+  return (
+    <div>
+      <label className={styles.label} htmlFor={id}>
+        {label}
+      </label>
+      {id === "description" ? (
+        <textarea
+          className={styles.textarea}
+          id={id}
+          {...props}
+          {...field}
+        ></textarea>
+      ) : (
+        <input className={styles.input} id={id} {...props} {...field} />
+      )}
+
+      {meta.touched && meta.error && (
+        <div style={{ color: "red" }}>
+          <ErrorMessage name={field.name} />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default InputHandler;
