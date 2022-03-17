62289d2ce7aa2375e473e3d126920a2ec9a6ac0e
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-17 23:44:41 +0200

    feature : protective routes

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,23 @@
-import React from "react";
-import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import React, { useContext } from "react";
+import { FaSignInAlt, FaSignOutAlt } from "react-icons/fa";
+import AuthContext from "../context/AuthContext";
 import Link from "next/link";
 import styles from "../styles/Header.module.css";
 import Search from "./Search";
+
 const Header = () => {
+  const { user, logout } = useContext(AuthContext);
+
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
         <Link href="/">
-          <a>Music Events</a>
+          <a>DJ Events</a>
         </Link>
       </div>
+
       <Search />
+
       <nav>
         <ul>
           <li>
@@ -19,29 +25,40 @@ const Header = () => {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-                <Link href='/events/add'>
+          {user ? (
+            // If logged in
+            <>
+              <li>
+                <Link href="/events/add">
                   <a>Add Event</a>
                 </Link>
               </li>
               <li>
-                <Link href='/account/login'>
-                  <a className="btn-secondary btn-icon" ><FaSignInAlt/> Login</a>
-                </Link>
-              </li>
-              <li>
-                <Link href='/account/dashboard'>
+                <Link href="/account/dashboard">
                   <a>Dashboard</a>
                 </Link>
               </li>
               <li>
                 <button
                   onClick={() => logout()}
-                  className='btn-secondary btn-icon'
+                  className="btn-secondary btn-icon"
                 >
                   <FaSignOutAlt /> Logout
                 </button>
               </li>
+            </>
+          ) : (
+            // If logged out
+            <>
+              <li>
+                <Link href="/account/login">
+                  <a className="btn-secondary btn-icon">
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
