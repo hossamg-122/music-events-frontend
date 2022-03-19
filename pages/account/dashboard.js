b99ae1ebf7da31e44a49acc0f0cf82f033479287
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-20 00:00:36 +0200

    feature : dashboard page

---
+++ pages/account/dashboard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../../components/Layout'
+const dashboard = () => {
+  return (
+    <Layout title="User Dashboard">
+ <h1>dashboard</h1>
+    </Layout>
+   
+  )
+}
+
+export default dashboard
