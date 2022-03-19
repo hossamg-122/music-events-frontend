f9ca4de92de8697b2ec4a58eb9e817cd58faeae0
hossamg-122 <79874817+hossamg-122@users.noreply.github.com>
2022-03-20 00:01:14 +0200

    delete

--- pages/api/hello.js
+++
@@ -1,5 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-// Api Routes can't be used with next export
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
